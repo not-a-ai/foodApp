7023b1b88c661534e58c13a779e34283ddf162eb
Aléxia <alexia.cassia09@gmail.com>
2024-05-04 16:32:12 -0400

    feat: add category item and component

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -1,5 +1,16 @@
-const CategoryList = () => {
-  return (  );
+import { db } from "../_lib/prisma";
+import CategoryItem from "./ui/category-item";
+
+const CategoryList = async () => {
+  const categorias = await db.category.findMany({});
+  
+  return (
+    <div className="flex overflow-x-scroll gap-3">
+      {categorias.map((category) => (
+      <CategoryItem key={category.id} category={category}/>
+      ))}
+    </div>
+  );
 }
  
 export default CategoryList;
