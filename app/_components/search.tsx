b1d72cc4b5ac99789a48ab24228b42cba647b2ac
Aléxia <alexia.cassia09@gmail.com>
2024-05-08 06:20:43 -0400

    feat: ensere restaurants can be search

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,18 +1,36 @@
+"use client";
+
 import { SearchIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { FormEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Search = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const handleSearchSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (!search) {
+      return;
+    }
+    router.push(`/restaurants?search=${search}`);
+  };
   return (
-    <div className="flex gap-2">
+    <form className="flex gap-2" onSubmit={handleSearchSubmit}>
       <Input
         placeholder="Buscar restaurantes"
         className="border-none bg-slate-100"
+        onChange={handleChange}
+        value={search}
       />
-      <Button size="icon">
+      <Button type="submit" size="icon">
         <SearchIcon size={16} />
       </Button>
-    </div>
+    </form>
   );
 };
 
